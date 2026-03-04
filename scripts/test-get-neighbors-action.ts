621325cf117a015a3afacbdface8b10dd94e34a6
Erich <eric.haynie@gmail.com>
2026-03-03 23:04:43 -0600

    Fix relative import path in test script

--- scripts/test-get-neighbors-action.ts
+++ scripts/test-get-neighbors-action.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { getNeighbors } from "./app/actions/neighbors";
+import { getNeighbors } from "../app/actions/neighbors";
 
 async function run() {
     const communityId = "488d50b6-1458-40bf-a417-2a1f64852410";
